dc5b5b6f080e3f33e2f1994f1e49d012e3d16c49
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2020-08-16 14:03:51 -0400

    Update rossbmatheny.py

--- rossbmatheny.py
+++ rossbmatheny.py
@@ -1,6 +1,7 @@
 import dominate
 import os
 import ntpath
+import functions
 
 from dominate.tags import *
 
@@ -38,28 +39,7 @@ with doc:
 			h2('Work', _class='section')
 			
 			with div(_class='center-content'):
-			
-				for entry in os.scandir(img_dir):
-					if (entry.path.endswith(".jpg")
-							or entry.path.endswith(".png")) and entry.is_file():
-						if entry.path.endswith(".jpg"):
-							img_name = ntpath.basename(entry.path).replace(".jpg","")
-						elif entry.path.endswith(".png"):
-							img_name = ntpath.basename(entry.path).replace(".png","")
-						if (img_name.startswith("a_")
-							or img_name.startswith("b_")
-							or img_name.startswith("c_")
-							or img_name.startswith("d_")
-							or img_name.startswith("e_")
-							or img_name.startswith("f_")
-							or img_name.startswith("g_")
-							or img_name.startswith("h_")
-							or img_name.startswith("i_")
-							or img_name.startswith("j_")
-							or img_name.startswith("k_")):
-							img_name = img_name[2:]
-						with div():
-							img(_class='u-max-full-width', src=entry.path, alt=img_name.capitalize())
+				functions.img_loop(img_dir)	
 
 		with div(_class='container'):
 			h2('Contact', _class="section")
