66a43e347b6dddaf43726eb52572993f944a742a
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2021-06-16 22:56:23 -0400

    testing

--- test/page.js
+++ test/page.js
@@ -1,21 +1,19 @@
 let myCards = document.querySelectorAll('.content');
 let rail = document.querySelector('.rail');
-
-rail.addEventListener('click', function(self) {
     
-    if (self.target.className === 'content') {
-        self.target.velocity({ 'border-color': ['blue', 'white']});
-    } else if (self.target.parentNode.className === 'content') {
-        self.target.parentNode.velocity({ 'border-color': ['blue', 'white']});
-    }
-    
-    console.log(self.target.parentNode);
-    self.stopPropagation();
-    
-});
-
-
-
-
-
-/*     Velocity(myCard, {'border-color': ['blue', 'white']}); */
+    document.addEventListener('click', function(self) {
+        
+        if (self.target.className === 'content') {
+            self.target.velocity({ 'border-color' : ['blue', 'white']});
+        } else if (self.target.parentNode.className === 'content') {
+            self.target.parentNode.velocity({ 'border-color' : ['blue', 'white']});
+        } else {
+            myCards.forEach( function(myCard) {
+                    myCard.velocity({ 'border-color' : 'white' });
+            });
+        }
+        
+        console.log(self.target);
+        /*self.stopPropagation();*/
+        
+    });
