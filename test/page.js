12f4d42cf7198584f64aff940391c1b7fe9453d6
brownbeard12 <31408027+brownbeard12@users.noreply.github.com>
2021-06-17 07:24:28 -0400

    Update page.js

--- test/page.js
+++ test/page.js
@@ -1,35 +1,35 @@
 let myCards = document.querySelectorAll('.content');
 let rail = document.querySelector('.rail');
     
-    document.addEventListener('click', function(self) {
+document.addEventListener('click', function(self) {
+    
+    var ind;
+    
+    if (self.target.className === 'content') {
+        self.target.velocity({ 'border-color' : ['red', 'white']}, {'duration' : '100'});
+    } else if (self.target.parentNode.className === 'content') {
+        self.target.parentNode.velocity({ 'border-color' : ['red', 'white']}, {'duration' : '100'});
+    };
+    
+    myCards.forEach( function(myCard, index) {
         
-        var ind;
-        
-        if (self.target.className === 'content') {
-            self.target.velocity({ 'border-color' : ['red', 'white']}, {'duration' : '100'});
-        } else if (self.target.parentNode.className === 'content') {
-            self.target.parentNode.velocity({ 'border-color' : ['red', 'white']}, {'duration' : '100'});
+        if (self.target === myCard || self.target.parentNode === myCard) {
+            ind = index;
         };
-        
-        myCards.forEach( function(myCard, index) {
-            
-            if (self.target === myCard || self.target.parentNode === myCard) {
-                ind = index;
-            };
 
-        });
-        
-        for( i = 0; i < myCards.length; i++) {
-            if (i === ind) {
-                continue;
-            }
-            myCards[i].velocity({ 'border-color' : 'white' }, {'duration' : '100'})
-        };            
-            
-
-        
-        console.log(self.target, self.target.parentNode, ind);
-                
-        self.stopPropagation();
-        
     });
+    
+    for( i = 0; i < myCards.length; i++) {
+        if (i === ind) {
+            continue;
+        }
+        myCards[i].velocity({ 'border-color' : 'white' }, {'duration' : '100'})
+    };            
+        
+
+    
+    console.log(self.target, self.target.parentNode, ind);
+            
+    self.stopPropagation();
+    
+});
